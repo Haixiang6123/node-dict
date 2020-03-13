2b8bfd22705409d0add963f8165448a5682592b9
Haixiang <haixiang6123@gmail.com>
2020-03-12 18:16:37 -0700

    Init project

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+// https://fanyi-api.baidu.com/api/trans/vip/translate
+import * as https from 'https'
+import * as querystring from 'querystring'
+import md5 = require('md5')
+
+export const translate = (word) => {
+  const appId = '....'
+  const appSecret = '...'
+  const salt = Math.random()
+  const sign = md5(appId + word + salt + appSecret)
+
+  const query: string = querystring.stringify({
+    q: word,
+    from: 'en',
+    to: 'zh',
+    appid: appId,
+    salt,
+    sign
+  })
+
+  const options = {
+    hostname: 'api.fanyi.baidu.com',
+    port: 443,
+    path: '/api/trans/vip/translate?' + query,
+    method: 'GET'
+  }
+
+  const req = https.request(options, (res) => {
+    console.log('状态码:', res.statusCode)
+    console.log('请求头:', res.headers)
+
+    res.on('data', (d) => {
+      process.stdout.write(d)
+    })
+  })
+
+  req.on('error', (e) => {
+    console.error(e)
+  })
+  req.end()
+}
